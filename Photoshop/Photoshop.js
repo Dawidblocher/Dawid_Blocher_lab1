e9eef65e96c1fef0d8651a1c9512938da573e77f
Blocher <blocher1997@gmail.com>
2019-11-19 18:26:37 +0100

    v1.2 photoshop

--- Photoshop/Photoshop.js
+++ Photoshop/Photoshop.js
@@ -1,21 +1,27 @@
 class Photoshop {
     constructor(targetElementId) {
         this.canvas = document.querySelector('#' + targetElementId)
+        this.canvas.width = window.innerWidth - 200;
         this.ctx = canvas.getContext('2d')
         this.canvas.addEventListener('touchmove', (e) => this.onTouchMove(e))
         this.canvas.addEventListener('touchstart', (e) => this.onTouchMove(e))
         this.brushShapeName = 'square';
         this.brushColor = '#000';
         this.brushSize = 10;
+        this.selectedImg = './img/testObraz.jpg';
 
+    }
+    setSizeCanvas(e) {
+        this.canvas.width = e.target.innerWidth - 200;
     }
 
     onTouchMove(e) {
         // pobierz pędzel
         this.brushShape = new Brush(this.brushShapeName, this.brushColor, this.brushSize);
         // ustal położenie
-        let x = e.touches[0].clientX - this.canvas.offsetLeft;
-        let y = e.touches[0].clientY - this.canvas.offsetTop
+        let x = e.touches[0].clientX - this.canvas.offsetLeft - 8;
+        let y = e.touches[0].clientY - this.canvas.offsetTop - 8;
+        console.log(e.touches[0].clientX - this.canvas.offsetLeft);
         this.ctx.beginPath();
         this.ctx.fillStyle = this.brushShape.color;
         this.ctx.strokeStyle = this.brushShape.color;
@@ -27,13 +33,27 @@ class Photoshop {
                 this.ctx.arc(x, y, this.brushShape.size / 2, 0, 10 * Math.PI);
                 this.ctx.fill();
                 break;
+            case 'img':
+                this.drawImage(this.selectedImg, x, y)
+                break;
+            case 'picker':
+
+                this.pickColor(e);
+
+                break;
+
         }
         this.ctx.stroke();
-
     }
 
     setBrush(brushShape) {
         this.brushShapeName = brushShape;
+        if (this.brushShapeName === 'img') {
+            document.querySelector('.imgChoose').classList.toggle('active');
+        } else {
+            document.querySelector('.imgChoose').classList.remove('active');
+        }
+
     }
 
     setColorBrush(color) {
@@ -41,6 +61,7 @@ class Photoshop {
     }
 
     setSizeBrush(size) {
+        document.querySelector('.labelSize').innerHTML = `Size: ${size}`;
         this.brushSize = size;
     }
 
@@ -49,32 +70,59 @@ class Photoshop {
         this.ctx.fillRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
     }
 
-    drawImage(){
+    drawImage(img, x, y) {
+        if (document.querySelector('.imgChoose').classList[1] !== 'active')
+            return;
+        this.clearWorkspace()
         const image = new Image();
-        image.src = './testObraz.jpg';
+        image.src = img;
         image.addEventListener('load', () => {
-            this.ctx.drawImage(image,0,0)
+            this.ctx.drawImage(image, x, y);
         })
     }
 
-    darkenFilter(amount = 30){
-        const canvasData = this.ctx.getImageData(0,0,900,600);
-        console.log(amount)
-        for(let i = 0; i < canvasData.data.length; i+=4){
-                canvasData.data[i] -= amount
-                canvasData.data[i+1] -= amount
-                canvasData.data[i+2] -= amount         
+    darkenFilter(amount = 30) {
+        const canvasData = this.ctx.getImageData(0, 0, this.canvas.width, 600);
+        for (let i = 0; i < canvasData.data.length; i += 4) {
+            canvasData.data[i] -= amount
+            canvasData.data[i + 1] -= amount
+            canvasData.data[i + 2] -= amount
         }
         this.ctx.putImageData(canvasData, 0, 0)
     }
-    brightnesFilter(amount = 30){
-        const canvasData = this.ctx.getImageData(0,0,900,600);
-        console.log(amount)
-        for(let i = 0; i < canvasData.data.length; i+=4){
-                canvasData.data[i] += amount
-                canvasData.data[i+1] += amount
-                canvasData.data[i+2] += amount         
+
+    brightnesFilter(amount = 30) {
+        const canvasData = this.ctx.getImageData(0, 0, this.canvas.width, 600);
+        for (let i = 0; i < canvasData.data.length; i += 4) {
+            canvasData.data[i] += amount
+            canvasData.data[i + 1] += amount
+            canvasData.data[i + 2] += amount
         }
         this.ctx.putImageData(canvasData, 0, 0)
     }
+    contrastFilter(amount = 1.2) {
+        const canvasData = this.ctx.getImageData(0, 0, this.canvas.width, 600);
+        for (let i = 0; i < canvasData.data.length; i += 4) {
+            canvasData.data[i] *= amount
+            canvasData.data[i + 1] *= amount
+            canvasData.data[i + 2] *= amount
+        }
+        this.ctx.putImageData(canvasData, 0, 0)
+    }
+
+    blackAndWhiteFilter(amount = 1.2) {
+        const canvasData = this.ctx.getImageData(0, 0, this.canvas.width, 600);
+        for (let i = 0; i < canvasData.data.length; i += 4) {
+            let grayscale = canvasData.data[i] * .3 + canvasData.data[i + 1] * .59 + canvasData.data[i + 2] * .11;
+            canvasData.data[i] = grayscale
+            canvasData.data[i + 1] = grayscale
+            canvasData.data[i + 2] = grayscale
+        }
+        this.ctx.putImageData(canvasData, 0, 0)
+    }
+    chooseImg(img) {
+        this.selectedImg = img;
+    }
+
+
 }
--- Photoshop/main.js
+++ Photoshop/main.js
@@ -4,7 +4,8 @@ function appStart() {
     const myPs = new Photoshop('canvas');
     const btnColor = document.querySelector('#color');
     const btnSize = document.querySelector('#range');
-    const btnFile = document.querySelector('#file');
+    window.addEventListener('resize', (e) => myPs.setSizeCanvas(e))
+
     document
         .querySelector('#squareBtn')
         .addEventListener('click', () => {
@@ -17,11 +18,28 @@ function appStart() {
             myPs.setBrush('circle')
         })
 
+    document
+        .querySelectorAll('.selectImg').forEach(img => img.addEventListener('click', () => {
+            myPs.chooseImg(img.src);
+            document.querySelectorAll('.selectImg').forEach(item => item.classList.remove('imgActive'));
+            img.classList.add('imgActive');
+        }))
+
+    document
+        .querySelector('#imgBtn')
+        .addEventListener('click', () => {
+            myPs.setBrush('img')
+        })
+
+
     btnColor.addEventListener('change', () => myPs.setColorBrush(btnColor.value));
     btnSize.addEventListener('change', () => myPs.setSizeBrush(btnSize.value))
     document.querySelector('#clear').addEventListener('click', () => myPs.clearWorkspace());
     document.querySelector('#darken').addEventListener('click', () => myPs.darkenFilter());
     document.querySelector('#brightnes').addEventListener('click', () => myPs.brightnesFilter());
-    myPs.drawImage(btnFile.value);
-    
+    document.querySelector('#contrast').addEventListener('click', () => myPs.contrastFilter());
+    document.querySelector('#black').addEventListener('click', () => myPs.blackAndWhiteFilter());
+
+
+
 }
