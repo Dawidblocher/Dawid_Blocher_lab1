d338f80921fc5c01ad7d5035b1e54141ddf08932
Blocher <blocher1997@gmail.com>
2020-02-28 15:35:13 +0100

    edit projects file

--- WeatherApp/App.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', appStart);
-
-function appStart(){
-    const app = new WeatherApp('483a82149295d0154a809580a3d890f2');
-    const cityInput = document.querySelector('#city');
-    const searchBtn = document.querySelector('#search');
-    cityInput.addEventListener('change', () => app.handleChangeCity(cityInput.value));
-    searchBtn.addEventListener('click', () => app.getData());
-}
